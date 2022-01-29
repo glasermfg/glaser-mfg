3074f819860d5c27e1f47f857f9896a924334103
JD Fillmore <jd@infused.agency>
2022-01-29 14:45:00 -0500

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -21,7 +21,64 @@ const Contact = () => {
             {/* One */}
             <Row>
               <Col lg="12">
-                <p>content</p>
+                <p>
+                  Please fill out the contact form below and we'll get back to
+                  you as soon as possible.
+                </p>
+                <form
+                  name="contact"
+                  method="post"
+                  data-netlify="true"
+                  data-netlify-honeypot="bot-field"
+                >
+                  <div class="form-group">
+                    <input
+                      name="name"
+                      type="name"
+                      class="form-control"
+                      id="Name-field"
+                      aria-describedby="namehelp"
+                      aria-label="name field"
+                      placeholder="Full Name"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      name="email"
+                      type="email"
+                      class="form-control"
+                      id="Email-field"
+                      aria-label="email field"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      name="phone"
+                      type="phone"
+                      class="form-control"
+                      id="Phone-field"
+                      aria-describedby="urlhelp"
+                      aria-label="phone field"
+                      placeholder="Cell Phone Number"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <textarea
+                      name="message"
+                      class="form-control"
+                      id="Message-field"
+                      aria-label="message field"
+                      rows="3"
+                      placeholder="How can we help you?"
+                    ></textarea>
+                  </div>
+                  <button className="btn btn-secondary" type="submit">
+                    Submit
+                  </button>
+                  <input type="hidden" name="bot-field" />
+                  <input type="hidden" name="form-name" value="contact" />
+                </form>
               </Col>
             </Row>
           </Container>
